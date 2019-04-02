db20eafbbc2828cadda0999524929f807178b600
lab03 (SavitskyV) <sv@gmail.com>
2019-04-02 17:11:47 +0300

    code: вывод svg

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -49,21 +49,56 @@ vector<size_t> make_histogram (const vector<double>& numbers, size_t bin_count) 
 
 
 
-void show_histogram_text(const vector<size_t>& bins, size_t bins_count) {
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg width='" << width << "' height='" << height << "' "
+    << "viewBox='0 0 " << width << " " << height << "' "
+    << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void
+    svg_end() {
+        cout << "</svg>\n";
+    }
+
+/*void
+show_histogram_svg(const vector<size_t>& bins) {
+    svg_begin(400, 300);
+    svg_end();
+}
+*/
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
+}
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
+    cout << "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill = '"<<fill<<"'/>";
+}
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = 10 * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "black", "black");
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
+
+
+
+
+/*void show_histogram_text(const vector<size_t>& bins, size_t bins_count) {
     const size_t max_zvezdochki = 76;
     
-  /*  cerr << "Vvedite simvol risunok:\n";
-    string risunok;
-    getline(cin, risunok);
-    
-    cerr << "Vvedite simvol linii osi:\n";
-    string liniya_osi;
-    getline(cin, liniya_osi);
-   
-    cerr << "Vvedite simvol viravnivaniya podpisey:\n";
-    string viravnivanie_podpisei;
-    getline(cin, viravnivanie_podpisei);
-   */
+ 
     cerr << "Histogram:\n";
     bool koef;
     
@@ -129,7 +164,7 @@ void show_histogram_text(const vector<size_t>& bins, size_t bins_count) {
     }
     
 }
-
+*/
 int main()
 
 {
@@ -148,5 +183,7 @@ int main()
     
     const vector<size_t>  bins = make_histogram(numbers, bins_count);
     
-    show_histogram_text(bins, bins_count);
+   // show_histogram_text(bins, bins_count);
+    
+    show_histogram_svg(bins);
 }
